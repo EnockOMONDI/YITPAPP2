ed485f1deca91c9d139b7c739fd9654c34cd1273
John Irungu <johnirunguchege2000@gmail.com>
2026-06-08 20:57:54 +0300

    fixed login bug

---
+++ users/backends.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+class EmailOrUsernameBackend(ModelBackend):
+    """
+    Custom authentication backend that allows users to log in using either their
+    username or their email address.
+    """
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        
+        try:
+            # Check username or email case-insensitively
+            user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to mitigate timing attacks
+            UserModel().set_password(password)
+            return None
+        except UserModel.MultipleObjectsReturned:
+            # In case multiple users have the same email address, authenticate the first one
+            user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).first()
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
